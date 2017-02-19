1334348016e0594b92cb52dfc53105d87bf8ed80
Cody Rourick <codyrourick@gmail.com>
2017-02-18 23:23:00 -0800

    Add fadeIn to body element

--- myScript.js
+++ myScript.js
@@ -1,7 +1,3 @@
-$(function(){
-    $('li').click(function() {
-      $("li.active").removeClass("active");
-      $(this).addClass('active');
-    });
-    $("body").css("user-select", "none")
+$(document).ready(function(){
+    $("body").hide().fadeIn(4000);
 });
