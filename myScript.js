3886569ec7d03c82c7486ad00b40f0924375ed0b
Cody Rourick <codyrourick@gmail.com>
2017-03-12 14:22:10 -0700

    Add javascript for first name form validation

--- myScript.js
+++ myScript.js
@@ -1,3 +1,11 @@
  $(document).ready(function(){
      $('#wrapper').hide().fadeIn(3000);
+
+     function validateForm(){
+         var x = document.forms["myForm"]["firstNameInput"].value;
+         if(x == ""){
+             alert("Please Enter Your First Name");
+             return false;
+         }
+     }
 });
