0fb81f7b5bad75397db3452f2036d06bd8762c2d
Cody Rourick <codyrourick@gmail.com>
2017-03-13 19:06:29 -0700

    Removed || for javascript to test form

--- myScript.js
+++ myScript.js
@@ -3,8 +3,10 @@
 
      function validateForm(){
          var x = document.forms["myForm"]["firstNameInput"].value;
+         var y = document.forms["myForm"]["lastNameInput"].value;
+         var z = document.forms["myForm"]["emailInput"].value;
          if(x == ""){
-             alert("Please Enter Your First Name");
+             alert("Please Fill In The Required Boxes");
              return false;
          }
      }
