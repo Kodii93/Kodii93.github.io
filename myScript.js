7029c83f567eccf06d8836de01abbeba6ee6e48e
Cody Rourick <codyrourick@gmail.com>
2017-03-16 22:15:33 -0700

    Change portfolio section color

--- myScript.js
+++ myScript.js
@@ -1,13 +1,3 @@
  $(document).ready(function(){
      $('#wrapper').hide().fadeIn(3000);
-
-     function validateForm(){
-         var x = document.forms["myForm"]["firstNameInput"].value;
-         var y = document.forms["myForm"]["lastNameInput"].value;
-         var z = document.forms["myForm"]["emailInput"].value;
-         if(x == ""){
-             alert("Please Fill In The Required Boxes");
-             return false;
-         }
-     }
 });
